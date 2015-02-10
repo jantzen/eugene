744cbed3cb69a650c5100ed4d5f9c39355b2a940
Benjamin C. Jantzen <bjantzen@vt.edu>
2015-02-10 10:12:04 -0500

    Includes a working version of exponential growth system. May have messed up VABSytem.py, but we don't know what belongs there yet.

---
+++ TestVABClasses.py
@@ -0,0 +1,7 @@
+# TestVABClasses.py
+
+from VABSystemExpGrowth import *
+
+def test_sensors():
+   sys = VABSystemExpGrowth(1, 0.2)
+
--- VABSystem.py
+++ VABSystem.py
@@ -1,14 +1,9 @@
-#Interface for dealing with the (virtual or simulated) sensors and actuators. 
+# VABSystem.py
+
+""" Generic system class. Specifically child classes provide simulations or 
+methods for manipulating real-world systems.
+"""
+
 class VABSystem( object ):
-    def __init__(self, sensors, actuators):
-        _sensors = sensors;
-        _actuators = actuators;
-        _sensorData = [];
-    
-    def read(self):
-        for sensor in self._sensors:
-            self._sensorData.add(sensor.read())
-        #Then store the data from sensorData into the database
-        
-    def intervene(self, actuatorID, value):
-        self._actuators[actuatorID].act(value)
+    def __init__(self):
+        # fill in this details of this class by abstracting from the example, VABSystemExpGrowth.py
