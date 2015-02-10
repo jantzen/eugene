d42554e31178368437b5c8f8162d7ed16d0eec74
Benjamin C. Jantzen <bjantzen@vt.edu>
2015-02-10 12:07:06 -0500

    Moved a lot of uncessary files to "scrap".

--- VABSystem.py
+++
@@ -1,9 +0,0 @@
-# VABSystem.py
-
-""" Generic system class. Specifically child classes provide simulations or 
-methods for manipulating real-world systems.
-"""
-
-class VABSystem( object ):
-    def __init__(self):
-        # fill in this details of this class by abstracting from the example, VABSystemExpGrowth.py
