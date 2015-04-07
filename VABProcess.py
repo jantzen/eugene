0a7440eaa0db9b329db657eb95d951fe218e96af
Joseph Mehr <joe.p.mehr@gmail.com>
2015-04-07 09:21:31 -0400

    Changed the genetic algorithm to be iterative rather than recursive.  Also made it stochastically chose the mutations that occur and the functions that are passed on to the next generation (with the top x% of fit functions being guaranteed to stay).  These changes solved the empty nextGeneration case, since it is no longer possible for nextGeneration to be empty unless invalid inputs are given to the function

--- VABProcess.py
+++ VABProcess.py
@@ -76,13 +76,12 @@ def SymmetryGroup(interface, func, time_var, intervention_var, inductive_thresho
     return (sum/inductive_threshold)
     
 
-def GeneticAlgorithm(interface, current_generation, generation_average, time_var, intervention_var, inductive_threshold, time_interval, const_ranges, deck, generation_limit, generation):
+def GeneticAlgorithm(interface, current_generation, time_var, intervention_var, inductive_threshold, time_interval, const_ranges, deck, generation_limit, num_mutes, generation_size, percent_guaranteed):
     """ Genetic Algorithm to find functions which are most likely to be symmetries
     
         Parameters:
             interface: System interface that is used to modify/monitor the system
             current_generation: The current generation of the GA
-            generation_average: The average fitness of the last generation
             time_var: The sensor ID of the time sensor
             intervention_var: The sensor ID of the actuator
             inductive_threshold: The number of trials before generalizing
@@ -90,27 +89,55 @@ def GeneticAlgorithm(interface, current_generation, generation_average, time_var
             const_ranges: Ranges of possible values for the constants in the functions
             deck: Set of expressions used to mutate functions
             generation_limit: Maximum number of generations before ending
-            generation: The current generation
+            num_mutes: The number of mutations to apply to each member of the current generation
+            generation_size: The maximum allowable size of a single generation
+            percent_guaranteed: The top x% of fit functions are guaranteed to be passed to the next generation
     """
-    nextGeneration = []
-    fitnessTotal = 0
     
-    #Loop through all of the current-gen functions
-    for func in current_generation:
-        for func2 in deck:
-            #Combine the function with the functions in the deck in various ways
-            modifiedFuncs = allOperations(func, func2)
-            for function in modifiedFuncs:
+    for generation in range(0, generation_limit):
+        nextGeneration = []
+        #Loop through all of the current-gen functions
+        for func in current_generation:
+            for func2 in RandomSelection(deck, num_mutes):
+                #Combine the function with the functions in the deck in various ways
+                modifiedFunc = randomOperation(func, func2)
                 #Measure fitness
-                function._fitness = SymmetryGroup(interface, function, time_var, intervention_var, inductive_threshold, time_interval, const_ranges)
-                if function._fitness > generation_average:
-                    nextGeneration.append(function)
-                    #DEAL WITH EMPTY NEXT GENERATION CASE
-    for func in nextGeneration:
-        fitnessTotal += func._fitness
-    if generation > generation_limit:
-        return nextGeneration
-    return GeneticAlgorithm(interface, nextGeneration, fitnessTotal/len(nextGeneration), time_var, intervention_var, inductive_threshold, time_interval, const_ranges, deck, generation_limit, generation + 1)
+                modifiedFunc._fitness = SymmetryGroup(interface, modifiedFunc, time_var, intervention_var, inductive_threshold, time_interval, const_ranges)
+                nextGeneration.append(modifiedFunc)
+                        
+         #Remove the functions that will not be passed on to the next generation:
+                        
+        #Sort the next generation by fitness
+        comparison = lambda x: x._fitness
+        nextGeneration.sort(comparison, reverse=True)
+        if generation_size > percent_guaranteed * len(nextGeneration):
+            fitnessTotals = []
+            
+            #Preserve the most fit functions
+            reducedGeneration = nextGeneration[0:percent_guaranteed*len(nextGeneration)]
+            runningTotal = 0
+            
+            #Create a list of running totals, representing a weighted distribution
+            for func in nextGeneration:
+                runningTotal += func._fitness
+                fitnessTotals.append(runningTotal)
+                
+            #Fill the rest of the spots in the next generation
+            for i in range(0, generation_size - percent_guaranteed * len(nextGeneration)):
+                
+                #Choose a random number that is greater than the running total from the last of the guaranteed-to-pass-on functions
+                rand = random.random() * (fitnessTotals[len(fitnessTotals) - 1] - fitnessTotals[percent_guaranteed*len(fitnessTotals)])
+                
+                #Use the random number and the weighted distribution to select a function to preserve
+                for j in range(percent_guaranteed*len(nextGeneration), len(nextGeneration)):
+                    if fitnessTotals[i] > rand:
+                        reducedGeneration.append(fitnessTotals[i])
+                        fitnessTotals.remove(fitnessTotals[i])
+                        nextGeneration.remove(nextGeneration[i])
+            current_generation = reducedGeneration
+        else:
+            current_generation = nextGeneration
+        
     
 def BranchAndBound(interface, seed_func, time_var, intervention_var, inductive_threshold, time_interval, const_ranges, deck, complexity_limit):
     """ Exaustive search of possible functions generated with expressions from 
@@ -139,6 +166,7 @@ def BranchAndBound(interface, seed_func, time_var, intervention_var, inductive_t
     return possibleSymmetries
     
     
+      
 def allOperations(function1, function2):
     """Returns the result of each possible combination of functions 1 and 2
     """
@@ -149,3 +177,21 @@ def allOperations(function1, function2):
     exponentiated = function1
     exponentiated.Power(function2)
     return [added, multiplied, exponentiated]
+
+def randomOperation(function1, function2):
+    """Returns the result of each possible combination of functions 1 and 2
+    """
+    operation = random.randomint(0, 2)
+    if operation == 0:
+        return function1.add(function2)
+    elif operation == 2:
+        return function1.Multiply(function2)
+    else:
+        return function1.Exponentiate(function2)
+
+def RandomSelection(deck, num_selected):
+    """Returns num_selected number of elements of the list, deck
+    """
+    random.shuffle(deck)
+    return deck[0:num_selected]
+    
