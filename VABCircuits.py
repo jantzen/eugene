f8d7f9221f37569e4d3a7580b4d2ed3a9023b98b
canis-diogenus <Caitlinp.vt@gmail.com>
2015-11-16 13:45:00 -0500

    """Minor changes to VABCircuits
    Combined dictionaries for phi functions and A_i's and made minor changes
    to the classes."""

--- VABCircuits.py
+++ VABCircuits.py
@@ -11,62 +11,55 @@ class CircuitRow(object):
     # Provides data corresponding to a particular circuit. 
     def __init__(self,circ):
         
-        # build a dictionary of Ais
+        # build a dictionary of circuit variables
         self.circ = circ
         
-        Ai = {1:[-2.017, 0, 0, 1, -1, 0, 0], 
-              2:[-2.017, 0, 0, -1, -1, 0, 0],
-              3:[0, 0, -2.8, 0, 1, 1, 0],
-              4:[-.44, 0, -2, 0, 0, 1, -1]}
-
-
-        phi = {1: lambda x:x**2,
-               2: lambda x:x**2,
-               3: lambda x:x**2,
-               4: lambda x:x**2}
+        Ais = {1:[-2.017, 0, 0, 1, -1, 0, 0, lambda x:x**2], 
+                       2:[-2.017, 0, 0, -1, -1, 0, 0, lambda x:x**2],
+                       3:[0, 0, -2.8, 0, 1, 1, 0, lambda x:x**2],
+                       4:[-.44, 0, -2, 0, 0, 1, -1, lambda x:x**2]}
                
  
-        init_x = {1: [0,0,1],
+        init_xs = {1: [0,0,1],
                   2: [0,0,-1],
                   3: [.5,-1,1],
                   4: [0,0,0]}
-                  
+        
+        # just checking the dictionary
+        Ai = Ais.get(1)  
+        print Ai
+        print Ai[0]              
         
     def getinit_x(self):
-        circ = CircuitRow.circ
-        init_xs = CircuitRow._init_x
-        init_x = init_xs.get(circ)
-        return init_x
-    
-    def getAi(self):
-        circ = CircuitRow.circ
-        Ais = CircuitRow._Ai       
-        Ai = Ais.get(circ)
-        return Ai
- 
-    def getphi(self):
-        circ = CircuitRow.circ
-        phis = CircuitRow._phi
-        phi = phis.get(circ)
-        return phi
-  
+        circ = self.circ 
+        init_xs = self._init_xs
+        return init_xs.get(circ)
         
-    #now set up deriv to pass to odeint  
+    
+    def getAis(self):
+        circ = self.circ
+        Ais = self._Ais      
+        return Ais.get(circ)
+
+print vars(CircuitRow)  #just checking
+
+  
+#now set up deriv to pass to odeint  
     
 def deriv(x,t):
-        # returns derivatives of the array x
+    # returns derivatives of the array x
     circ = CircuitRow.circ     
-    Ai = CircuitRow.getAi(circ)
-    phi = CircuitRow.getphi(circ)
+    Ai = CircuitRow.getAis(circ)
+    phi = Ai[7]
     return np.array([x[1], x[2], Ai[0] * x[2] + Ai[1] * phi(x[2]) \
             + Ai[2] * x[1] + Ai[3] * phi(x[1]) + Ai[4] * x[0] \
             + Ai[5] * phi(x[0]) + Ai[6]])
-
 
 class JerkFunctions(object):
 # Produces a function based on the information for a particular CircuitRow.
 
     def __init__(self, circ):
+        self.circ = circ        
         circ = CircuitRow(circ)    
     
     def updatevoltage(self):   
@@ -77,6 +70,6 @@ class JerkFunctions(object):
         time = linspace(init_t, 60, 100)
         v = odeint(deriv,init_x,time)
         return v
-        print v
         
-
+    
+print vars(JerkFunctions) #checking
