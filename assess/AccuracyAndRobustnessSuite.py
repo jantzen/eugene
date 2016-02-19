0749656e5ba2b0429e4deea720d579228ba7068d
JP <2518gazewood@gmail.com>
2016-02-19 18:54:06 -0500

    SimpleNoiseExperiment!
            (And some cleaning of the packaging.)

--- assess/AccuracyAndRobustnessSuite.py
+++ assess/AccuracyAndRobustnessSuite.py
@@ -1,4 +1,5 @@
 # import matplotlib.pyplot as plt
+import pdb
 
 import sys
 import random
@@ -6,6 +7,8 @@ import numpy as np
 
 # sys.path.insert(0, 'C\vabacon')
 import eugene as eu
+from eugene.src.virtual_sys.growth_sys import LogisticGrowthModel
+
 """Accuracy and Robustness Suite
 
 Sets up four experiments demonstrating accuracy and robustness: one showing 
@@ -14,27 +17,49 @@ under noise with increasing deviations from normality; one showing performance
 as it varies with increasing similarity among systems; one showing performance
 as it changes with variation in r-values versus k values.""" 
 
+def BuildModel(data_frame, sys_id, epsilon=0):
+    model = eu.compare.BuildSymModel(data_frame, 1, 2, sys_id, epsilon)
+
+    return model
+#!!!!!!!! Change BuildSymModel params^^
+
 ########
 #Functions:
+
+
 
 def SimpleNoiseExperiment():
    # basic noise experiment
     standard_devs = [0.1, 1., 2., 4., 6., 8., 10.]
+    standard_devs = [0.1, 0.15, 0.2, 0.25, 0.3, 0.31]
     sys1 = [1, 1, 1, 1, 1, 1, 0]
     sys2 = [1, 1, 2, 1, 1, 1, 0]
     twoSys = [sys1, sys2]
 
     noise_bracket = []
     for noiselevel in standard_devs: 
+        #keep track of known answers
+        answers = []
+        #bracket each of eugene's answers
         bracket = []
-        first = random.randint(1,2)
-        second  = random.randint(1,2)
         #10 trials per noise level
+        #EUGENE (really numpy..) breaks at standard_devs >= 0.32 b/c
+        #b/c target values = 'outofrange'
         for x in range(10):
+            first = random.randint(0,1)
+            second  = random.randint(0,1)
+            if (first == second):
+                answers.append(0)
+            else:
+                answers.append(1)
             bracket.append(LGExperiment(noiselevel, 
                                         twoSys[first],
                                         twoSys[second]))
+            
         noise_bracket.append(bracket)
+        print '\n' 'answr:', answers
+        print '\n', 'noise:', bracket, '\n'
+
 
     #temporary return value
     return noise_bracket
@@ -69,8 +94,8 @@ def LGExperiment(noise_stdev, sp1, sp2):
       noise_stdev reflects the (square root of the) variance of our Gaussian
       distribution. In a noise experiment, we steadily increase this value. 
 
-      sp1 = parameters for first system
-      sp2 = parameters for second system
+      sp1 = system parameters 1.
+      sp2 = system parameters 2.
 
     @return-------------------------------------------
       ....meaningful data.... list of classes determined out of the
@@ -80,7 +105,7 @@ def LGExperiment(noise_stdev, sp1, sp2):
 
 
     ### local variables
-    con = 0.0 #CONFUSED    (where does Janzten get these numbers from??)
+    con = 5.0 #CONFUSED    (where does Janzten get these numbers from??)
     epsilon=10**(-4)
     resolution=[300,3]
     ###
@@ -90,7 +115,7 @@ def LGExperiment(noise_stdev, sp1, sp2):
     isensor = eu.sensors.VABTimeSensor([])
     iact = eu.actuators.VABVirtualTimeActuator()
     #target sensor & actuator
-    tsensor = eu.sensors.PopulationSensor([10**(-23), 10**23], noiselevel, 
+    tsensor = eu.sensors.PopulationSensor([10**(-23), 10**23], noise_stdev, 
                                           False)
     #tsensor = eu.sensors.PopulationSensor([-10.**23, 10.**23], noise_stdev, 
     #                                      False)   ^XOR ?
@@ -104,19 +129,20 @@ def LGExperiment(noise_stdev, sp1, sp2):
     systems = []
     #LGModel().__init__(self, r, init_x, K, alpha, beta, gamma, init_t)
     systems.append(LogisticGrowthModel(sp1[0], sp1[1], sp1[2], sp1[3],
-                                       sp1[4], sp1[5], sp1[6])
+                                       sp1[4], sp1[5], sp1[6]))
+
 
     systems.append(LogisticGrowthModel(sp2[0], sp2[1], sp2[2], sp2[3],
-                                       sp2[4], sp2[5], sp2[6])
+                                       sp2[4], sp2[5], sp2[6]))
 
-    
+
     interfaces = []
     for sys in systems:
         interfaces.append(eu.interface.VABSystemInterface(sensors, actuators, sys))
     #blaahhhh - in case you were wondering how we find ROI, we're not 
     #           supposed to: this is where Collin's AutoRangeDet thing 
     #           comes in.
-    ROI = dict([(1,[con, 20.]), (2, [con, 65.])])
+    ROI = dict([(1,[0., 20.]), (2, [con, 65.])])
     ###    
 
 
@@ -126,15 +152,14 @@ def LGExperiment(noise_stdev, sp1, sp2):
         data.append(eu.interface.TimeSampleData(1, 2, iface, ROI, resolution))
     ###
 
-    ### Eugene, use your magic powers of witchcraft & wizardry! :P hah...
+    ### Line143-144 is where it breaks
     models = []
     for sys_id, data_frame in enumerate(data):
         models.append(BuildModel(data_frame, sys_id, epsilon))
 
-    classes = eu.categorize.Classify(range(len(systems)), models)
-    ### Hell yeah!
+    same0diff1 = eu.compare.CompareModels(models[0], models[1])
 
-    return classes
+    return same0diff1
 
 #####################################################################
 
