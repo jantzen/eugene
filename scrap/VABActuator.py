bb036560bfb81f355c1cc1827910c7b5c7f04367
Benjamin C. Jantzen <bjantzen@vt.edu>
2016-05-30 15:50:29 -0400

    Cleanup
    A large number of files and directories were removed from the EUGENE
    project directory, and added to a new repository. If you don't see a
    file you're look ing for, that's where it is.

--- scrap/VABActuator.py
+++
@@ -1,4 +0,0 @@
-#This is an abstract class which other actuator classes will inherit from
-class VABSensor( object ):
-    def act(self, value):
-        raise NotImplementedError("'act' not yet implemented")
--- scrap/VABSensor.py
+++
@@ -1,4 +0,0 @@
-#This is an abstract class which other sensor classes will inherit from
-class VABSensor( object ):
-    def read(self):
-        raise NotImplementedError("'read' not yet implemented")
