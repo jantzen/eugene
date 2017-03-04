fbea31303f7c55f9e5fb96fce21a00d3a988a485
Benjamin C. Jantzen <bjantzen@vt.edu>
2017-03-04 10:46:10 -0500

    Finished setup.py file.

--- setup.py
+++ setup.py
@@ -18,25 +18,40 @@ setup(name='eugene',
 
       version='0.1.0',
 
+      description='Tools for automated scientific discovery',
+      long_description=long_description,
+
+      url='http://www.ratiocination.org/',
+
       author='Benjamin C. Jantzen',
       author_email='bjantzen@vt.edu',
+
       maintainer='Benjamin C. Jantzen',
       maintainer_email='bjantzen@vt.edu',
-      url='http://www.ratiocination.org/',
-      download_url='https://github.com/jantzen/eugene', 
-      description='tools for automated scientific discovery',
-      requires=['numpy', 'scipy'],
 
-      classifiers=['Development Status :: 1 - Dev',
-                   'Intended Audience :: Developers & Scientists',
-                   'License :: ...',
-                   'Programming Language :: Python',
-                   'Topic :: automated scientific discovery',],
-      platforms='',
-      license='',
+      download_url='https://github.com/jantzen/eugene', 
+
+      license='MIT',
+
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence', 
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+                   ],
+
+      keywords='discovery kinds dynamical system-identification natural-kinds'
 
       #specifying package as 'eugene' instead of root ('') installs
       #the package as a folder, rather than dumping the contents of that folder
+
+      install_requires=['numpy', 'scipy'],
 
       packages=['eugene', 'eugene.src', 
                 'eugene.src.connect', 
