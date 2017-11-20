b2ab609dd86e3cd06bd43117922883c0239f7dbd
Benjamin C. Jantzen <bjantzen@vt.edu>
2017-11-20 16:05:30 -0500

    Minor additions to resampling and tweaks to LVDSim
    The biggest change is the addition of a Gaussian method to resample.py.

--- eugene/tests/test_sampling.py
+++ eugene/tests/test_sampling.py
@@ -8,7 +8,8 @@ from scipy import stats
 def test_uniform():
     data = np.random.normal(size=(1000,2))
     resampled_data = resample.uniform(data)
-    h = np.histogram(resampled_data[:,0])
+#    h = np.histogram(resampled_data[:,0])
+    assert len(resampled_data) == 10 * len(data)
 
     # compute K-S test for data
     [D_data, p_val_data] = stats.kstest(data[:,0], 'norm')
@@ -24,6 +25,31 @@ def test_uniform():
     # distribution
     [D_resamp_uni, p_val_resamp_uni] = stats.kstest(resampled_data[:,0],
             'uniform')
+
+    assert p_val_resamp_uni < 0.05
+
+def test_gaussian():
+    data = np.random.random_sample(size=(1000,2))
+    resampled_data = resample.gaussian(data)
+#    h = np.histogram(resampled_data[:,0])
+    assert len(resampled_data) == 10 * len(data)
+
+    # compute K-S test for data
+    [D_data, p_val_data] = stats.kstest(data[:,0], 'uniform')
+
+    assert p_val_data > 0.05
+
+    # compute K-S test to show resampled_data does _not_ fit a uniform
+    # distribution
+    [D_resamp_norm, p_val_resamp_norm] = stats.kstest(resampled_data[:,0],
+            'uniform')
+
+    assert p_val_resamp_norm < 0.05
+
+    # compute K-S test to show resampled_data is indistinguishable from normal
+    # distribution
+    [D_resamp_uni, p_val_resamp_uni] = stats.kstest(resampled_data[:,0], 
+            'norm')
 
     assert p_val_resamp_uni < 0.05
 
