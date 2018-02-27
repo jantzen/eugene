5506de83c2d8c35d57d718ee2dc1d012291ca05a
Colin Shea-Blymyer <c0lin@vt.edu>
2018-02-27 13:25:18 -0500

    Parallelized generation of data for LVD_map.
    * This might be a good block of code to break out of LVD_map.
    * The distance matrix isn't representative of our space, needs a new vis.

--- eugene/scripts_demos/LVD_map.py
+++ eugene/scripts_demos/LVD_map.py
@@ -1,29 +1,39 @@
 # LVD_map.py
 
-from eugene.src.tools.LVDSim import *
+from joblib import Parallel, delayed
+from eugene.src.tools.LVDSim import simData, tuplesToBlocks, energyDistanceMatrixParallel
+import multiprocessing
 from eugene.src.tools.alphaBetaGrid import *
 from multiprocessing import cpu_count
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def map_data(init_pops, trans_pops, caps, max_time, num_times, overlay, depth):
+def map_data(init_pops, trans_pops, caps, max_time, num_times, depth):
     num_cores = multiprocessing.cpu_count() - 2
     list_of_points = lv_map_points(alpha_steps=depth, beta_steps=depth)
     params = Parallel(n_jobs=num_cores)(delayed(point_to_param)(point, init_pops, trans_pops, caps) for point in list_of_points)
 
-    data = simData(params, max_time, num_times, overlay, range_cover=False)
+    data = Parallel(n_jobs=num_cores)(delayed(simData)([param], max_time, num_times, overlaid, stochastic_reps=5, range_cover=False) for param in params)
 
-    blocks = tuplesToBlocks(data)
+    # data = simData(params, max_time, num_times, overlay, range_cover=False)
+    new_data = []
+    for c in data:
+        new_data.append(c[0])
+
+    blocks = tuplesToBlocks(new_data)
 
     dmat = energyDistanceMatrixParallel(blocks)
     return dmat
 
 
 def point_to_param(point, init_pops, trans_pops, caps):
-    param = [point[0], caps, point[1], init_pops, trans_pops]
+    sigma = [0.1, 0.1, 0.1, 0.1]
+    param = [point[0], caps, point[1], sigma, init_pops, trans_pops]
     return param
 
+def overlaid(x):
+    return x
 
 # def map_demo():
     #do something
@@ -31,11 +41,11 @@ def point_to_param(point, init_pops, trans_pops, caps):
 
 if __name__ == '__main__':
     # overlay = lambda x: np.mean(x, axis=1)
-    overlay = lambda x: x
+    # overlay = lambda x: x
     k = np.array([100., 100., 100., 100.])
     init_pops = np.array([5., 5., 5., 5.])
     trans_pops = np.array([8., 8., 8., 8.])
-    dmat = map_data(init_pops, trans_pops, k, 100., 1000., overlay, 500.)
-    plt.imshow(dmat, cmap='Blues', interpolation='nearest')
+    dist_mat = map_data(init_pops, trans_pops, k, 100., 10., 6.)
+    plt.imshow(dist_mat, cmap='hot', interpolation='nearest')
     plt.show()
-    np.savetxt("LVD_map_dmat.txt", dmat, fmt='%.5f')
+    np.savetxt("LVD_map_dmat.txt", dist_mat, fmt='%.5f')
