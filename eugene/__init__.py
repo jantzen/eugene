d1fdfac715fee6d6b5c9776987de2ede79b94d63
Benjamin C. Jantzen <jantzen@ratiocination.org>
2020-11-24 11:42:48 -0500

    Adjusted imports in __init__.py to include stochastic L-V.

--- eugene/__init__.py
+++ eugene/__init__.py
@@ -9,6 +9,7 @@ from .src.virtual_sys import growth_sys
 from .src.virtual_sys import chaotic_circuits
 from .src.virtual_sys import LotkaVolterra2D
 from .src.virtual_sys import LotkaVolterraND
+from .src.virtual_sys import LotkaVolterraSND
 from .src.auxiliary import probability
 from .src.auxiliary import csv_to_dataframe
 from .src.auxiliary import sampling
